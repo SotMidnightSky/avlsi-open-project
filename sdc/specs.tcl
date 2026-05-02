80b8191270a81284ec836b6337fdd85ef1c75736
Thomas Jenkins <thomas000@me.com>
2026-05-01 23:40:30 -0500

    removed unused clock

--- sdc/specs.tcl
+++ sdc/specs.tcl
@@ -6,10 +6,10 @@ set_clock_latency 0.3 clk
 set_input_delay 2.0 -clock clk [all_inputs]
 set_output_delay 1.65 -clock clk [all_outputs]
 # specify clk_serial
-create_clock clk_serial -period 14 -waveform {0 7}
-set_clock_latency 0.3 clk_serial
-set_input_delay 2.0 -clock clk_serial [all_inputs]
-set_output_delay 1.65 -clock clk_serial [all_outputs]
+# create_clock clk_serial -period 14 -waveform {0 7}
+# set_clock_latency 0.3 clk_serial
+# set_input_delay 2.0 -clock clk_serial [all_inputs]
+# set_output_delay 1.65 -clock clk_serial [all_outputs]
 # specify loads
 set_load 0.1 [all_outputs]
 set_max_fanout 1 [all_inputs]
